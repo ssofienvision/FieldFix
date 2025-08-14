17e91d8faf6f6a68072748c2fa1f8b160e4421fa
ssofienvision <sofien.smaali@gmail.com>
2025-08-14 16:44:33 -0400

    Create server.js

---
+++ apps/assets-warranty/server.js
@@ -0,0 +1,10 @@
+import http from "http";
+const PORT = process.env.PORT || 3000;
+http.createServer((req, res) => {
+  if (req.url === "/health") {
+    res.writeHead(200, { "content-type": "application/json" });
+    return res.end(JSON.stringify({ status: "ok", service: "assets-warranty" }));
+  }
+  res.writeHead(200, { "content-type": "text/plain" });
+  res.end("assets-warranty service is running\n");
+}).listen(PORT, () => console.log("assets-warranty listening on", PORT));
